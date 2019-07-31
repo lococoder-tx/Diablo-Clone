c1a90064ea48e6649e25b9e5d23e1e748bf3e1ae
Elijah Gaytan <elijahgaytan@utexas.edu>
2019-07-30 21:08:49 -0500

    first commit of this project

---
+++ Assets/Scripts/CameraTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTester : MonoBehaviour
+{
+    // Simple Camera Follow Script
+    public Transform target;
+
+    public float smoothSpeed;
+    public Vector3 offSet;
+    
+    void LateUpdate()
+    {
+        transform.position = target.position + offSet;
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private NavMeshAgent thisNavAgent;
+
+    private Ray lastRay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisNavAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //left mouse button clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100)
+    }
+
+    private void MoveToCursor()
+    {
+        //draw ray from camera position to wherever cursor is pointing
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            thisNavAgent.destination = hit.point;
+        }
+
+    }
+}
